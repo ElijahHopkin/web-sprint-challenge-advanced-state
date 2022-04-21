50bed3062b6dfd0088c44081e20f6fedef139028
enhopkin@gmail.com <enhopkin@gmail.com>
2022-04-21 16:36:38 -0600

    Wheel component complete

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,7 +1,20 @@
+import {
+  INPUT_CHANGE,
+  MOVE_CLOCKWISE, 
+  MOVE_COUNTERCLOCKWISE, 
+  RESET_FORM, 
+  SET_INFO_MESSAGE, 
+  SET_QUIZ_INTO_STATE, 
+  SET_SELECTED_ANSWER
+} from './action-types'
 // ❗ You don't need to add extra action creators to achieve MVP
-export function moveClockwise() { }
+export function moveClockwise() { 
+  return({type: MOVE_CLOCKWISE})
+}
 
-export function moveCounterClockwise() { }
+export function moveCounterClockwise() {
+  return({type: MOVE_COUNTERCLOCKWISE})
+ }
 
 export function selectAnswer() { }
 
--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,24 +1,57 @@
 // ❗ You don't need to add extra reducers to achieve MVP
 import { combineReducers } from 'redux'
+import {
+  INPUT_CHANGE,
+  MOVE_CLOCKWISE, 
+  MOVE_COUNTERCLOCKWISE, 
+  RESET_FORM, 
+  SET_INFO_MESSAGE, 
+  SET_QUIZ_INTO_STATE, 
+  SET_SELECTED_ANSWER
+} from './action-types'
 
 const initialWheelState = 0
-function wheel(state = initialWheelState, action) {
-  return state
+export function wheel(state = initialWheelState, action) {
+  switch(action.type) {
+    case MOVE_CLOCKWISE:
+      if(state <5) {
+        return (state +1)
+      }else{
+        return (state=0)
+      }
+    case MOVE_COUNTERCLOCKWISE:
+      if(state>0) {
+        return (state -1)
+      }else{
+        return (state=5)
+      }
+    default:
+      return state
+  }
 }
 
 const initialQuizState = null
-function quiz(state = initialQuizState, action) {
-  return state
+export function quiz(state = initialQuizState, action) {
+  switch(action.type) {
+    default:
+      return state
+  }
 }
 
 const initialSelectedAnswerState = null
-function selectedAnswer(state = initialSelectedAnswerState, action) {
-  return state
+export function selectedAnswer(state = initialSelectedAnswerState, action) {
+  switch(action.type) {
+    default:
+      return state
+  }
 }
 
 const initialMessageState = ''
-function infoMessage(state = initialMessageState, action) {
-  return state
+export function infoMessage(state = initialMessageState, action) {
+  switch(action.type) {
+    default:
+      return state
+  }
 }
 
 const initialFormState = {
@@ -26,8 +59,11 @@ const initialFormState = {
   newTrueAnswer: '',
   newFalseAnswer: '',
 }
-function form(state = initialFormState, action) {
-  return state
+export function form(state = initialFormState, action) {
+  switch(action.type) {
+    default:
+      return state
+  }
 }
 
 export default combineReducers({ wheel, quiz, selectedAnswer, infoMessage, form })
